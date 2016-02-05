78f585b2b2d97b89419e3b0171bf09b1b1c8fc7f
David Findlay <david@woodypointcomms.com.au>
2016-02-05 21:35:47 +1000

    Empty module created
    Module now works, based on Dropbox module. No content in module yet.

--- embeddedmedia/EmbeddedMediaModule.php
+++ embeddedmedia/EmbeddedMediaModule.php
@@ -1,11 +1,19 @@
 <?php
 
+namespace humhub\modules\embeddedmedia;
+
+use Yii;
+use humhub\modules\user\models\User;
+use humhub\modules\space\models\Space;
+use humhub\modules\content\components\ContentContainerModule;
+use humhub\modules\content\components\ContentContainerActiveRecord;
+
 /**
  * EmbeddedMediaModule Base Module Class
  *
  * @author David Findlay <davidjwfindlay@gmail.com>
  */
-class EmbeddedMediaModule extends HWebModule
+class EmbeddedMediaModule extends ContentContainerModule
 {
     
 }
